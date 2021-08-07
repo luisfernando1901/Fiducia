e198c164f9c0ab595258cca5e0787855cd4756a0
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-08-07 18:34:07 -0500

    prod environment modificado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQa5gFDx6uc6Mic7fcy2KGzIa2cL8WjSs",
+    authDomain: "fiducia-789f3.firebaseapp.com",
+    projectId: "fiducia-789f3",
+    storageBucket: "fiducia-789f3.appspot.com",
+    messagingSenderId: "595474452982",
+    appId: "1:595474452982:web:ec5b0d2ccf28b87cedb7d7"
+  }
 };
