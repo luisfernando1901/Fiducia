cd46a6f66c8c628ba37d881af6f6c7b5f66741b7
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-08-07 19:41:30 -0500

    environment updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDQa5gFDx6uc6Mic7fcy2KGzIa2cL8WjSs",
-    authDomain: "fiducia-789f3.firebaseapp.com",
-    projectId: "fiducia-789f3",
-    storageBucket: "fiducia-789f3.appspot.com",
-    messagingSenderId: "595474452982",
-    appId: "1:595474452982:web:ec5b0d2ccf28b87cedb7d7"
-  }
 };
 
 /*
